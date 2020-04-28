e1030c7d3dfdaa7713ac59db0c03e2387fd48a42
Hiroki-kt <katayama.hiroki.kb3@is.naist.jp>
2020-04-28 10:58:06 +0900

    confirm real robot movement

--- docker_install_ubuntu.sh
+++ docker_install_ubuntu.sh
@@ -2,9 +2,11 @@
 
 # install docker ce (ref. https://docs.docker.com/engine/install/ubuntu/)
 sudo apt-get update
-sudo apt-get install apt-transport-https\
+sudo apt-get install \
+  apt-transport-https\
   ca-certificates\
-  curl gnupg-agent\
+  curl\
+  gnupg-agent\
   software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
